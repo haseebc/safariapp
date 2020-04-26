f69105babd8f3633179cb3ccd4448764041b0792
haseebc <haseebchaudhary@gmail.com>
2020-04-26 21:03:07 +0200

    models a bit messedup

--- app/controllers/fotos_controller.rb
+++ app/controllers/fotos_controller.rb
@@ -1,9 +1,22 @@
 class FotosController < ApplicationController
     def new
     end
+    
+    def show
+        @foto = Foto.find(params[:id])
+    end
 
     def create
-        render plain: params[:foto].inspect
+        @foto = Foto.new(foto_params)
+ 
+        @foto.save
+        redirect_to @foto
+        
+    end
+
+    private
+        def foto_params
+            params.require(:foto).permit(:title, :text, :location)
     end
 
 end
